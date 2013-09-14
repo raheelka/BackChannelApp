39952eadbfb10edc325ef42a1d6dfd058388e42f
Raheel Kazi <raheelka@gmail.com>
2013-09-14 13:06:54 -0400

    Full name feature added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :full_name
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def full_name
+     @full_name = @current_user.first_name + " " + @current_user.last_name
+  end
+
 
 end

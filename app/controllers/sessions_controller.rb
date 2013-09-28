d4b14f2fcd0b4fdc8d663027e3363489585a4d5c
Raheel Kazi <raheelka@gmail.com>
2013-09-28 16:56:51 -0400

    Login takes to all post page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email_or_username], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to :controller => "posts", :action => "index" , :notice => "Logged in!"
     else
       flash.now.alert = "Invalid Login! "
       render "new"
